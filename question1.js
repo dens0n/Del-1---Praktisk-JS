a8e8bff50cbe5ff75dfad4b56b545a76442c7628
dens0n <156082076+dens0n@users.noreply.github.com>
2024-02-19 11:37:40 +0100

    .

--- question1.js
+++ question1.js
@@ -6,21 +6,19 @@
 const car = {
     name: "Volvo",
     speed: 120,
-  };
-  
-  function addIsFast(car) {
-    // Din kod här
-    
-    return {...car, isFast: car.speed>100}
+};
 
-  }
+function addIsFast(car) {
+    // Din kod här
+
+    return { ...car, isFast: car.speed > 100 };
+}
 
 // 1.2 (1p) Skapa tre variabler name, speed och isFast genom
 // destructuring av objektet som returneras från addIsFast.
 
 // Din kod här:
-const {name, speed, isFast} = car
-
+const { name, speed, isFast } = addIsFast(car);
 
 console.log(addIsFast(car)); // Ska logga { name: "Volvo", speed: 120, isFast: true }
 console.log(name, speed, isFast); // Ska logga "Volvo", 120, true
